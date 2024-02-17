b58d3b05466e4d47ce88965885a0fa55f682d3dd
Chandra Bhushan <87900167+chandra-bhushan01@users.noreply.github.com>
2024-02-17 10:38:22 +0000

    conversion and readablity

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,84 @@
+// let score = null
+
+
+// console.log(typeof(score)) // can also be written as (typeof score)
+// console.log(typeof score) // can also be written as methord (typeof(score))
+
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+// //"33" => 33
+// //33abc => NaN (not a number) but data type of NaN is Number.
+// //true => 1; false => 0
+
+// let isLoggedin = NaN
+
+// let bolleanIsLoggedIn = Boolean(isLoggedin);
+
+// console.log(bolleanIsLoggedIn)
+
+// //1 => ture; 0 => false
+// //"" => false
+// //"hitesh" => true
+// // null => false
+// // undefined => false
+// //Nan =>false
+
+
+// let someString  = true
+// let stringNumber = String(someString)
+
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// //33 =>string
+// //null=> string 
+// //Nan => stirng 
+// // true =>string
+
+
+
+
+/************************************************Operations*******************************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue) // give the exact negetive value.
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // => 2 raised to power 2
+// console.log(2/2)
+// console.log(2%2)
+
+
+
+let str1 = "hello"
+let str2 = " chandra"
+let str3  = str1+str2
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 54+2;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)

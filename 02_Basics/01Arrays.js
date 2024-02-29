39814c2e2501fc6eb86ed873fd35431c5dfe3440
chandra-bhushan01 <chandrabhushanbgp321@gmail.com>
2024-03-01 01:38:26 +0530

    arrays end and some functions

--- 02_Basics/01Arrays.js
+++ 02_Basics/01Arrays.js
@@ -8,12 +8,12 @@ const myHeros = ['shaktimaan', 'naagraj']
 const myArr = new Array(1,2,3,4,5)
 
 
-console.log(myArray[2])
+// console.log(myArray[2])
 myArray.length
 
 
 //Array Methords
-console.log("original: "+myArr)
+// console.log("original: "+myArr)
 // myArr.push(6)    //->Insert at the end of the Array
 // myArr.pop()      //->removes the last element of an array.
 // myArr.unshift(9)    //-> enter element to the front
@@ -27,10 +27,52 @@ console.log("original: "+myArr)
 // console.log(typeof newArray)
 
 //Slice and Splice
-console.log("A",myArr);
-const myn1 = myArr.slice(1,4)
-console.log("B", myn1)
-const myn2 = myArr.splice(1,4)
-console.log("C",myn2)
+// console.log("A",myArr);
+// const myn1 = myArr.slice(1,4)
+// console.log("B", myn1)
+// const myn2 = myArr.splice(1,4)
+// console.log("C",myn2)
+
+
+
+
+
+const marvelHeros = ['iron man',"thor","captain amarica"]
+const dcHeros = ['superman',"hawk","wonder women"]
+
+
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros)
+
+
+
+// const newarr = marvelHeros.concat(dcHeros)
+// console.log(newarr)
+
+const spreadArray = [...dcHeros,...marvelHeros]
+
+// console.log(spreadArray)
+
+const arryOther = [1,2,3,[4,5,6],[7,8,[9,10]]]
+const flatArray = arryOther.flat(1)
+// console.log(flatArray)
+
+
+
+console.log(Array.isArray("chandra bhushan"))
+console.log(Array.from("chandra bhushan"))
+console.log(Array.from({name:"chandra"})) //intresting
+
+
+
+const a= 3920
+const b = 97
+const c = 2324
+console.log(Array.of(a,b,c))
+
+
+
+
+
 
 
